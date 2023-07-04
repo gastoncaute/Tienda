f65b2a35fbc65125c76077dccc4a51efbed14c3c
gastoncaute <gastoncaute@gmail.com>
2023-07-03 21:43:17 -0300

    API productos

--- Pagina_principal/Pagina_principal.js
+++ Pagina_principal/Pagina_principal.js
@@ -8,17 +8,14 @@ import './Categorias_menu/categorias_menu.css';
 import { setupCategorias } from './Categorias_menu/categorias_menu.js';
 setupCategorias(document.getElementById('btn-categorias'));
 
-import './Precios-menu/precios-menu.css'
+import './Precios-menu/precios-menu.css';
 import { setupPrecios } from './Precios-menu/precios-menu.js';
-setupPrecios(document.getElementById('btn-precios'))
+setupPrecios(document.getElementById('btn-precios'));
 
-import './user_menu/user_menu.css'
+import './user_menu/user_menu.css';
 import { setupUser } from "./user_menu/user_menu.js";
-setupUser(document.getElementById('user-btn'))
+setupUser(document.getElementById('user-btn'));
 
-
-const btnAbrirLista = document.getElementById("btn_listadeproductos")
-
-btnAbrirLista.addEventListener("click", () => {
-    window.location.href="./productos/productos.html"
-})
+import './productos/productos.css'
+import { setUpProductos } from "./productos/productos.js";
+setUpProductos(document.getElementById('btn-lista-productos'))
--- Pagina_principal/productos/productos.js
+++ Pagina_principal/productos/productos.js
@@ -1,17 +1,38 @@
-
-
-async function obetenerProductos () {
+export function setUpProductos() {
+    const btnAbrirLista = document.getElementById("btn-lista-productos")
+    btnAbrirLista.addEventListener("click", () => {
+        window.location.href="./productos/productos.html"
+    })
+    class UserCard extends HTMLElement {
+        constructor() {
+            super()
+            this.title = this.getAttribute("title") ?? "Articulo desconocido"
+            this.price = this.getAttribute("precie") ?? "Precio desconocido"
+            const html = `
+                <li>
+                    <p>Nombre: ${this.title}<p/>
+                    <p>Precio: ${this.price}<p/>
+                    <p>Descripcion: ${this.description}<p/>
+                    <p>Categoria: ${this.category}<p/>
+                    <p>Imagen: ${this.image}<p/>
+                    <p>Valoracion: ${this.rate}<p/>
+                <li/>
+            `
+            this.insertAdjacentHTML("beforeend", UserCard)
+        }
+    }
+    async function obetenerProductos() {
     const res = fetch(`https://fakestoreapi.com/products/category/electronics`)
     const datos = await res.json() 
     return datos
-}
-
-const listaDeProductos = document.getElementById("listadeproductos")
-obetenerProductos()
-.then( (productos) =>{
-    productos.foreach((producto) => {
-        const itemProducto= document.createElement("li")
-        itemProducto.innerHTML= `<productcard nombre= "${producto.title}"/>` 
-        listaDeProductos.appendChild(itemProducto)
+    }
+    const listaDeProductos = document.getElementById("lista-productos")
+    obetenerProductos()
+    .then((productos) =>{
+        productos.foreach((producto) => {
+            const itemProducto = document.createElement("li")
+            itemProducto.innerHTML = `<productcard nombre="${producto.title}"/>` 
+            listaDeProductos.appendChild(itemProducto)
+        })
     })
-})
+}
