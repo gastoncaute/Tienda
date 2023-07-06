57ca1f8c118de489e5e2f6fdcb1ef18be0775822
gastoncaute <gastoncaute@gmail.com>
2023-07-06 12:54:22 -0300

    Se agregan los datos a html

--- Pagina_principal/productos/productos.js
+++ Pagina_principal/productos/productos.js
@@ -1,16 +1,49 @@
-async function obtenerProductos() {
-    const listaProductos = document.getElementById('lista-productos');
-    fetch(`https://fakestoreapi.com/products/category/electronics`)
-    .then(response => response.json())
-    .then(data => {
-    listaProductos.innerHTML = JSON.stringify(data);
-})}
-const listaDeProductos = document.getElementById("lista-productos")
-obtenerProductos()
-.then((productos) =>{
-    productos.forEach((producto) => {
-        const itemProducto = document.createElement("li")
-        itemProducto.innerHTML = `<ProductCard nombre="${producto.title}" price="${producto.price}" descripcion="${producto.description}" categoria="${producto.category}" imagen="${producto.image}" rate="${producto.rate}"/>` 
-        listaDeProductos.appendChild(itemProducto)
+class ProductCard extends HTMLElement {
+    constructor() {
+        super()
+        this.title = this.getAttribute("title") ?? "Nombre desconocido"
+        this.price = this.getAttribute("price") ?? "Precio descopmocido"
+        this.description = this.getAttribute("description") ?? "Descripción desconocida"
+        this.category = this.getAttribute("category") ?? "Categoría descopmocida"
+        this.image = this.getAttribute("image") ?? "Imagen desconocida"
+        this.rating = this.getAttribute("rating") ?? "Valoración descopmocida"
+        const html = `
+            <li>
+                <p class="product-title">Título: ${this.title}</p>
+                <p class="product-image">${this.image}</p>
+                <p class="product-description">Descripción: ${this.description}</p>
+                <p class="product-category">Categoría: ${this.category}</p>
+                <p class="product-price">Precio: ${this.price}</p>
+                <p class="product-rating">Valoración: ${this.rating}</p>
+            </li>
+        `   
+        this.insertAdjacentHTML("beforeend", html)
+    }
+}
+
+customElements.define("user-card", ProductCard)
+
+const listaProductos = document.getElementById('lista-productos');
+let contenidoDelProducto = []
+
+fetch(`https://fakestoreapi.com/products/category/electronics`)
+    .then((res) => res.json())
+    .then((datos) => {
+        contenidoDelProducto = datos
+        console.log(contenidoDelProducto)
+        obtenerProductos()
     })
-})
+    .catch((err) => console.log(err))
+
+function obtenerProductos() {
+    contenidoDelProducto.forEach((datos) => {
+        const ul = document.createElement("ul")
+        for(const dato of Object.values(datos)) {
+            const li= document.createElement("li")
+            li.innerHTML = `<user-card title="${datos.title}" price="${datos.price}" description="${datos.description}" category="${datos.category}" image="${datos.image}" rating="${datos.rating.rate}"/>`
+            ul.appendChild(li)
+            listaProductos.appendChild(ul)
+        }
+    })
+}
+obtenerProductos()
