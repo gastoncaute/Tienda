3262a297c86f18d81c7ea549842cb5bc0bd4e640
gastoncaute <gastoncaute@gmail.com>
2023-07-10 14:00:02 -0300

    Se agrega ventana datos de usuario

--- Pagina_principal/user_menu/user_menu.js
+++ Pagina_principal/user_menu/user_menu.js
@@ -9,4 +9,18 @@ export function setupUser() {
             userMenu.classList.remove('toggle');
         }
     })
+    const datosDeUsuario = localStorage.getItem("datos-de-usuario");
+    if (datosDeUsuario) {
+        const usuario = JSON.parse(datosDeUsuario);
+        const nombreUsuario = usuario.completeName;
+        const usernameUsuario = usuario.username
+        const emailUsuario = usuario.email;
+        const nombreUsuarioElement = document.getElementById("user-menu-fullname");
+        const usernameUsuarioElemnt = document.getElementById("user-menu-username")
+        const emailUsuarioElement = document.getElementById("user-menu-email");
+        nombreUsuarioElement.textContent = nombreUsuario;
+        usernameUsuarioElemnt.textContent = usernameUsuario;
+        emailUsuarioElement.textContent = emailUsuario;
+    }
+
 }
