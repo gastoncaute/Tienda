ff0e0c6ffc7a99c2c232ec8825fea302b7c4e061
gastoncaute <gastoncaute@gmail.com>
2023-07-09 22:22:14 -0300

    Se arreglan los productos

--- Pagina_principal/Precios-menu/precios-menu.js
+++ Pagina_principal/Precios-menu/precios-menu.js
@@ -6,26 +6,4 @@ export function setupPrecios() {
         preciosMenu.classList.toggle('toggle')
         categoriasMenu.classList.remove('toggle')
     })
-
-    const btnPrecioMayor = document.getElementById('btn-precio-mayor')
-    const btnPrecioMenor = document.getElementById('btn-precio-menor')
-
-    let listaDeProductos = [
-    { nombre: "Producto 1", precio: 10 },
-    { nombre: "Producto 2", precio: 5 },
-    { nombre: "Producto 3", precio: 8 },
-    { nombre: "Producto 4", precio: 3 },
-    { nombre: "Producto 5", precio: 12 }
-    ]
-
-    btnPrecioMayor.addEventListener("click", () => {
-        // Ordenar de mayor a menor
-        const preciosOrdenados = listaDeProductos.sort((a, b) => b.precio - a.precio)
-        console.log(preciosOrdenados)
-    })
-    btnPrecioMenor.addEventListener("click", () => {
-        // Ordenar de menor a mayor
-        const preciosOrdenados = listaDeProductos.sort((a, b) => a.precio - b.precio)
-        console.log(preciosOrdenados)
-    })
 }
--- Pagina_principal/productos/productos.js
+++ Pagina_principal/productos/productos.js
@@ -1,9 +1,4 @@
 export function setUpProductos() {
-    const btnAbrirLista = document.getElementById("btn-lista-productos")
-    btnAbrirLista.addEventListener("click", () => {
-        window.location.href="./productos/productos.html"
-    })
-
     class ProductCard extends HTMLElement {
         constructor() {
             super()
@@ -14,13 +9,13 @@ export function setUpProductos() {
             this.image = this.getAttribute("image") ?? "Imagen desconocida"
             this.rating = this.getAttribute("rating") ?? "Valoración descopmocida"
             const html = `
-                <li>
+                <li class="product-articules">
                     <p class="product-title">Título: ${this.title}</p>
-                    <p class="product-image">${this.image}</p>
                     <p class="product-description">Descripción: ${this.description}</p>
                     <p class="product-category">Categoría: ${this.category}</p>
                     <p class="product-price">Precio: ${this.price}</p>
                     <p class="product-rating">Valoración: ${this.rating}</p>
+                    <img src="${this.image}" alt="Imagen del producto" class="product-image">
                 </li>
             `   
             this.insertAdjacentHTML("beforeend", html)
@@ -31,25 +26,74 @@ export function setUpProductos() {
     const listaProductos = document.getElementById('lista-productos');
     let contenidoDelProducto = []
 
-    fetch(`https://fakestoreapi.com/products/category/electronics`)
+
+
+    fetch(`https://fakestoreapi.com/products`)
         .then((res) => res.json())
         .then((datos) => {
             contenidoDelProducto = datos
-            console.log(contenidoDelProducto)
+            obtenerTodosLosProductos()
             obtenerProductos()
         })
         .catch((err) => console.log(err))
 
-    function obtenerProductos() {
+    function obtenerTodosLosProductos() {
+        const ul = document.createElement("ul");
         contenidoDelProducto.forEach((datos) => {
-            const ul = document.createElement("ul")
-            for(const dato of Object.values(datos)) {
-                const li= document.createElement("li")
-                li.innerHTML = `<product-card title="${datos.title}" price="${datos.price}" description="${datos.description}" category="${datos.category}" image="${datos.image}" rating="${datos.rating.rate}"/>`
-                ul.appendChild(li)
-                listaProductos.appendChild(ul)
-            }
-        })
+            const li = document.createElement("li");
+            li.innerHTML = `<product-card title="${datos.title}" price="${datos.price}" description="${datos.description}" category="${datos.category}" image="${datos.image}" rating="${datos.rating.rate}"/>`;
+            ul.appendChild(li);
+        });
+        listaProductos.appendChild(ul);
     }
-    obtenerProductos()
+    obtenerTodosLosProductos()
+    function obtenerProductos(categoria) {
+        fetch(`https://fakestoreapi.com/products/category/${categoria}`)
+            .then((res) => res.json())
+            .then((datos) => {
+                const ul = document.createElement("ul");
+                datos.forEach((producto) => {
+                    const li = document.createElement("li");
+                    li.innerHTML = `<product-card title="${producto.title}" price="${producto.price}" description="${producto.description}" category="${producto.category}" image="${producto.image}" rating="${producto.rating.rate}"/>`;
+                    ul.appendChild(li);
+                });
+                listaProductos.appendChild(ul);
+            })
+            .catch((err) => console.log(err));
+    }
+    const btnCategorias = document.querySelectorAll(".btn-categoria");
+    btnCategorias.forEach((btn) => {
+        btn.addEventListener("click", (evento) => {
+            const categoria = evento.target.getAttribute("data-categoria");
+            listaProductos.innerHTML = "";
+            obtenerProductos(categoria);
+        });
+    });
+
+    const btnPrecioMayor = document.getElementById('btn-precio-mayor')
+    const btnPrecioMenor = document.getElementById('btn-precio-menor')
+    btnPrecioMayor.addEventListener("click", () => {
+        ordenarProductos(true);
+        });
+    btnPrecioMenor.addEventListener("click", () => {
+        ordenarProductos(false);
+    });
+
+    function ordenarProductos(deMayorAMenor) {
+        listaProductos.innerHTML = "";
+        const productosOrdenados = contenidoDelProducto.sort((a, b) => {
+            if (deMayorAMenor) {
+                return b.price - a.price;
+            } else {
+                return a.price - b.price;
+            }
+        });
+        const ul = document.createElement("ul");
+        productosOrdenados.forEach((producto) => {
+            const li = document.createElement("li");
+            li.innerHTML = `<product-card title="${producto.title}" price="${producto.price}" description="${producto.description}" category="${producto.category}" image="${producto.image}" rating="${producto.rating.rate}"/>`;
+            ul.appendChild(li);
+        });
+    listaProductos.appendChild(ul);
+    }
 }
